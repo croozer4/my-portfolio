d10d558dc24fe752e4ccfff2418bb8aa6da56ab8
croozer4 <konradkul12@gmail.com>
2025-10-07 20:51:42 +0200

    Fixed Timeline

--- src/components/MyExperience.vue
+++ src/components/MyExperience.vue
@@ -9,90 +9,113 @@ defineProps({
 <template>
     <!-- Tekst My Projects -->
     <div class="w-full bg-base-200">
-    <div class="max-w-screen-xl mx-auto bg-base-200">
-        <!-- ciemny kolor -->
-        <h1 class="text-xl lg:text-5xl font-bold my-8 pl-8 text-white-800 pt-8">My Experience</h1>
+        <div class="max-w-screen-xl mx-auto">
+            <!-- ciemny kolor -->
+            <h1 class="!text-5xl font-bold my-8 pl-8 text-white pt-8">My Experience</h1>
 
-<ul class="timeline timeline-vertical lg:timeline-horizontal">
-    <li>
-        <div class="timeline-start">02.2018</div>
-        <div class="timeline-middle">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-            </svg>
-        </div>
-        <div class="timeline-end timeline-box text-center">KB Systems</div>
-        <hr />
-    </li>
-    <li>
-        <hr />
-        <div class="timeline-start">07.2018</div>
-        <div class="timeline-middle">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-            </svg>
-        </div>
-        <div class="timeline-end timeline-box text-center">Chip7</div>
-        <hr />
-    </li>
-    <li>
-        <hr />
-        <div class="timeline-start">01.2020</div>
-        <div class="timeline-middle">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-            </svg>
-        </div>
-        <div class="timeline-end timeline-box text-center">TK Laptops</div>
-        <hr />
-    </li>
-    <li>
-        <hr />
-        <div class="timeline-start">04.2020</div>
-        <div class="timeline-middle">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-            </svg>
-        </div>
-        <div class="timeline-end timeline-box">IT Technician (TZN)</div>
-        <hr />
-    </li>
-    <li>
-        <hr />
-        <div class="timeline-start">07.2023</div>
-        <div class="timeline-middle">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-            </svg>
-        </div>
-        <div class="timeline-end timeline-box">KB Systems</div>
-        <hr />
-    </li>
-    <li>
-        <hr />
-        <div class="timeline-start">01.2024</div>
-        <div class="timeline-middle">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-            </svg>
-        </div>
-        <div class="timeline-end timeline-box">Engineer (PCz)</div>
-        <hr />
-    </li>
-    <li>
-        <hr />
-        <div class="timeline-start">Currently</div>
-        <div class="timeline-middle">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-            </svg>
-        </div>
-        <div class="timeline-end timeline-box">Master’s Degree (PCz)</div>
-    </li>
-</ul>
+            <div class="w-full flex p-8 pl-0">
 
+                <ul class="timeline timeline-vertical lg:timeline-horizontal w-full flex justify-between">
+                    <li class="flex-1">
+                        <div class="timeline-start">02.2018</div>
+                        <div class="timeline-middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                class="h-5 w-5">
+                                <path fill-rule="evenodd"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <div class="timeline-end timeline-box text-center text-sm">KB Systems</div>
+                        <hr />
+                    </li>
+                    <li class="flex-1">
+                        <hr />
+                        <div class="timeline-start">07.2018</div>
+                        <div class="timeline-middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                class="h-5 w-5">
+                                <path fill-rule="evenodd"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <div class="timeline-end timeline-box text-center text-sm">Chip7</div>
+                        <hr />
+                    </li>
+                    <li class="flex-1">
+                        <hr />
+                        <div class="timeline-start">01.2020</div>
+                        <div class="timeline-middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                class="h-5 w-5">
+                                <path fill-rule="evenodd"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <div class="timeline-end timeline-box text-center text-sm">TK Laptops</div>
+                        <hr />
+                    </li>
+                    <li class="flex-1">
+                        <hr />
+                        <div class="timeline-start">04.2020</div>
+                        <div class="timeline-middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                class="h-5 w-5">
+                                <path fill-rule="evenodd"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <div class="timeline-end timeline-box text-center text-sm">IT Technician (TZN)</div>
+                        <hr />
+                    </li>
+                    <li class="flex-1">
+                        <hr />
+                        <div class="timeline-start">07.2023</div>
+                        <div class="timeline-middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                class="h-5 w-5">
+                                <path fill-rule="evenodd"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <div class="timeline-end timeline-box text-center text-sm">KB Systems</div>
+                        <hr />
+                    </li>
+                    <li class="flex-1">
+                        <hr />
+                        <div class="timeline-start">01.2024</div>
+                        <div class="timeline-middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                class="h-5 w-5">
+                                <path fill-rule="evenodd"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <div class="timeline-end timeline-box text-center text-sm">Engineer (PCz)</div>
+                        <hr />
+                    </li>
+                    <li class="flex-1">
+                        <hr />
+                        <div class="timeline-start">Currently</div>
+                        <div class="timeline-middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                class="h-5 w-5">
+                                <path fill-rule="evenodd"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <div class="timeline-end timeline-box text-center text-sm">Master’s Degree (PCz)</div>
+                    </li>
+                </ul>
+            </div>
 
-    </div>
+        </div>
     </div>
 
 </template>
@@ -101,4 +124,13 @@ defineProps({
 .read-the-docs {
     color: #888;
 }
+
+.timeline hr {
+  background-color: tomato !important;
+}
+
+.timeline svg {
+  color: tomato !important;
+}
+
 </style>
